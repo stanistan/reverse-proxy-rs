5456446b39be8e96664010391798e39f8f81cc6f
Ben Linsay <blinsay@gmail.com>
2018-05-01 13:30:22 -0400

    holy shit, it works

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 #![allow(dead_code, unused_imports, unused_variables)]
-extern crate tokio_core;
 extern crate futures;
 extern crate hyper;
+extern crate tokio_core;
 extern crate url;
 
-use tokio_core::reactor::{Core, Handle};
 use futures::future::{AndThen, Future, FutureResult, OrElse};
-use hyper::header::{ContentLength, Headers};
+use futures::stream::Stream;
 use hyper::client::HttpConnector;
+use hyper::header::{ContentLength, Headers};
 use hyper::server::{Http, Response, Service};
 use hyper::{Client, Method, Request, StatusCode, Uri};
+use std::sync::Arc;
+use tokio_core::reactor::{Core, Handle};
 use url::{form_urlencoded, Url};
 
 struct Context {
@@ -25,7 +27,7 @@ enum ProxyError {
 }
 
 struct ReverseProxy {
-    client: Client<HttpConnector>
+    client: Arc<Client<HttpConnector>>,
 }
 
 impl Service for ReverseProxy {
@@ -36,23 +38,31 @@ impl Service for ReverseProxy {
     type Future = BoxFuture;
 
     fn call(&self, req: Request) -> Self::Future {
-        Box::new(proxy_incoming_request(&req, &self.client)
-            .and_then(proxy_outgoing_request)
-            .or_else(|err| {
-                println!("fuck: {:?}", err);
-                let response = Response::new()
-                    .with_status(StatusCode::InternalServerError);
-                Ok(response)
-            }))
+        let client = self.client.clone();
+        Box::new(
+            noop(req)
+                .and_then(|r| proxy_incoming_request(r, client))
+                .and_then(proxy_outgoing_request)
+                .or_else(|err| {
+                    println!("fuck: {:?}", err);
+                    let response = Response::new().with_status(StatusCode::InternalServerError);
+                    Ok(response)
+                }),
+        )
     }
 }
 
 // FIXME these should have better type names
+type HmmFuture = Box<Future<Item = Request, Error = hyper::Error>>;
 type BoxFuture = Box<Future<Item = Response, Error = hyper::Error>>;
+
+fn noop(request: Request) -> HmmFuture {
+    return Box::new(futures::future::ok(request));
+}
 
 // start handling an incoming https request from a client, translate it into an outgoing upstream
 // http(s) request, and return a future for that request
-fn proxy_incoming_request(request: &Request, client: &Client<HttpConnector>) -> BoxFuture {
+fn proxy_incoming_request(request: Request, client: Arc<Client<HttpConnector>>) -> BoxFuture {
     use std::str::FromStr;
     if request.method() != &Method::Get {
         let mut response = Response::new();
@@ -60,7 +70,7 @@ fn proxy_incoming_request(request: &Request, client: &Client<HttpConnector>) -> 
         return Box::new(futures::future::ok(response));
     }
 
-    let url = match get_target_url(request) {
+    let url = match get_target_url(&request) {
         Err(proxy_error) => {
             println!("{:?}", proxy_error); // debooglin
 
@@ -68,10 +78,13 @@ fn proxy_incoming_request(request: &Request, client: &Client<HttpConnector>) -> 
             response.set_status(StatusCode::BadRequest);
             return Box::new(futures::future::ok(response));
         }
-        Ok(url) => url
+        Ok(url) => url,
     };
 
-    let request = Request::new(Method::Get, Uri::from_str(&url.as_str()).expect("fuck this"));
+    let request = Request::new(
+        Method::Get,
+        Uri::from_str(&url.as_str()).expect("fuck this"),
+    );
     println!("proxy_incoming_request: {:?}", request);
     Box::new(client.request(request))
 }
@@ -84,9 +97,10 @@ fn proxy_outgoing_request(proxy_response: Response) -> BoxFuture {
     }
     println!("proxy_outgoing_request: {:?}", proxy_response);
 
-    Box::new(futures::future::ok(proxy_response.with_headers(Headers::new())))
+    Box::new(futures::future::ok(
+        proxy_response.with_headers(Headers::new()),
+    ))
 }
-
 
 fn get_target_url(request: &Request) -> Result<Url, ProxyError> {
     let query = match request.query() {
@@ -101,18 +115,33 @@ fn get_target_url(request: &Request) -> Result<Url, ProxyError> {
     }
 }
 
-fn build_proxy_service() -> ReverseProxy {
-    let core = Core::new().expect("error: no core for you");
-    let client = Client::configure()
-        .build(&core.handle());
-
-    ReverseProxy { client }
-}
-
 fn main() {
+    let mut core = Core::new().expect("error: no core for you");
     let addr = "127.0.0.1:3000".parse().unwrap();
-    let server = Http::new()
-        .bind(&addr, || Ok(build_proxy_service()))
+
+    let server_handle = core.handle();
+    let client_handle = core.handle();
+
+    let serve = Http::new()
+        .serve_addr_handle(&addr, &server_handle, move || {
+            Ok(ReverseProxy {
+                client: Arc::new(Client::configure().build(&client_handle)),
+            })
+        })
         .unwrap();
-    server.run().unwrap();
+
+    let h2 = server_handle.clone();
+    server_handle.spawn(
+        serve
+            .for_each(move |conn| {
+                h2.spawn(
+                    conn.map(|_| ())
+                        .map_err(|err| println!("serve err: {:?}", err)),
+                );
+                Ok(())
+            })
+            .map_err(|_| ()),
+    );
+
+    core.run(futures::future::empty::<(), ()>()).unwrap();
 }
